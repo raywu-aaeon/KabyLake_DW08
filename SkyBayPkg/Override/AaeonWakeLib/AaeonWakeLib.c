3716d2f783993b35f4729394a4f7ffa458a4bdc1
raywu <raywu@aaeon.com.tw>
2019-11-26 10:48:48 +0800

    [XI-BringUp] Bring Up Porting / Support PCIe Wake

--- SkyBayPkg/Override/AaeonWakeLib/AaeonWakeLib.c
+++ SkyBayPkg/Override/AaeonWakeLib/AaeonWakeLib.c
@@ -37,3 +37,47 @@ SmmSetTheStateToGoAfterG3 (SETUP_DATA *SetupData)
 	return Status;
 }
 //ray_override / [XI-BringUp] Bring Up Porting / AC Power Loss from PCH : Always ON / Added <<
+
+//ray_override / [XI-BringUp] Bring Up Porting / Support PCIe Wake / Added >>
+EFI_STATUS
+EFIAPI
+PciExpressWakeConfiguration (SETUP_DATA *SetupData)
+{
+	EFI_STATUS Status = EFI_SUCCESS;
+        UINT16  PmBase;
+
+        PmBase = SbGetAcpiBaseAddress();
+        // Disable Wake Event
+        // GPIO[27] Enable (LANWAKE_EN)
+        // PME_B0 Enable (PME_B0_EN)
+        // PCI Express Enable (PCI_EXP_EN)
+        // PCI Express Wake Disable (PCIEXP_WAKE_DIS)
+        IoWrite32( PmBase + R_PCH_ACPI_GPE0_EN_127_96 ,
+                IoRead32(PmBase + R_PCH_ACPI_GPE0_EN_127_96) & ~(B_PCH_ACPI_GPE0_EN_127_96_LAN_WAKE
+                                                        | B_PCH_ACPI_GPE0_EN_127_96_PME_B0
+                                                        | B_PCH_ACPI_GPE0_EN_127_96_PCI_EXP) ) ;
+        IoWrite16( PmBase + R_PCH_ACPI_PM1_EN ,
+                IoRead16(PmBase + R_PCH_ACPI_PM1_EN) | (B_PCH_ACPI_PM1_STS_PCIEXP_WAKE_STS) ) ;
+
+        // Clear Wake Status
+        IoWrite32( PmBase + R_PCH_ACPI_GPE0_STS_127_96 ,
+                IoRead32(PmBase + R_PCH_ACPI_GPE0_STS_127_96) & ~(B_PCH_ACPI_GPE0_STS_127_96_LAN_WAKE
+                                                        | B_PCH_ACPI_GPE0_STS_127_96_PME_B0
+                                                        | B_PCH_ACPI_GPE0_STS_127_96_PCI_EXP) ) ;
+        IoWrite16( PmBase + R_PCH_ACPI_PM1_STS ,
+                IoRead16(PmBase + R_PCH_ACPI_PM1_STS) | (B_PCH_ACPI_PM1_STS_PCIEXP_WAKE_STS) ) ;
+        // Enable Wake Event
+        // GPIO[27] Enable (LANWAKE_EN)
+        // PME_B0 Enable (PME_B0_EN)
+        // PCI Express Enable (PCI_EXP_EN)
+        // PCI Express Wake Disable (PCIEXP_WAKE_DIS)
+        IoWrite32( PmBase + R_PCH_ACPI_GPE0_EN_127_96 ,
+                IoRead32(PmBase + R_PCH_ACPI_GPE0_EN_127_96) | (B_PCH_ACPI_GPE0_EN_127_96_LAN_WAKE
+                                                        | B_PCH_ACPI_GPE0_EN_127_96_PME_B0
+                                                        | B_PCH_ACPI_GPE0_EN_127_96_PCI_EXP) ) ;
+        IoWrite16( PmBase + R_PCH_ACPI_PM1_EN ,
+                IoRead16(PmBase + R_PCH_ACPI_PM1_EN) & ~(B_PCH_ACPI_PM1_STS_PCIEXP_WAKE_STS) ) ;
+
+	return Status;
+}
+//ray_override / [XI-BringUp] Bring Up Porting / Support PCIe Wake / Added <<
