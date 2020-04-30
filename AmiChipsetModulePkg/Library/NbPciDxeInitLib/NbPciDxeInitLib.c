b0e1293b15b546d70692f19f817c3ca7dadb9520
raywu <raywu@aaeon.com.tw>
2020-04-30 15:09:23 +0800

    [DEBUG] TRACE ImageStart ... Corrected Build Failed

--- AmiChipsetModulePkg/Library/NbPciDxeInitLib/NbPciDxeInitLib.c
+++ AmiChipsetModulePkg/Library/NbPciDxeInitLib/NbPciDxeInitLib.c
@@ -295,15 +295,19 @@ DEBUG_RAYDEBUG((-1, "VbiosImageGuid = %g\n", VbiosImageGuid));
                    if (!EFI_ERROR (Status)) {
 //raydebug >>
 //VOID *ImageStart;
-DEBUG_RAYDEBUG((-1, "ImageStart  0x%X\n", (UINT8 *)((UINT32)ImageStart)));
+DEBUG_RAYDEBUG((-1, "(UINT8 *)((UINT32)ImageStart)  0x%X\n", (UINT8 *)((UINT32)ImageStart)));
+DEBUG_RAYDEBUG((-1, "(UINT8 )((UINT32)ImageStart)  0x%X\n", (UINT8 )((UINT32)ImageStart)));
 DEBUG_RAYDEBUG((-1, "TRACE ImageStart\n"));
 DEBUG_RAYDEBUG((-1, "ImageStart = 0x%X\n", ImageStart));
 DEBUG_RAYDEBUG((-1, "&ImageStart = 0x%X\n", &ImageStart));
 DEBUG_RAYDEBUG((-1, "(UINT32 *)ImageStart = 0x%X\n", (UINT32 *)ImageStart));
 DEBUG_RAYDEBUG((-1, "(UINT32 **)ImageStart = 0x%X\n", (UINT32 **)ImageStart));
 DEBUG_RAYDEBUG((-1, "((UINT32)ImageStart + 1) = 0x%X\n", ((UINT32)ImageStart + 1)));
-DEBUG_RAYDEBUG((-1, "**ImageStart = 0x%X\n", **ImageStart));
 
+
+/*
+
+*/
 //{
 //  UINT8 i ;
 //  
