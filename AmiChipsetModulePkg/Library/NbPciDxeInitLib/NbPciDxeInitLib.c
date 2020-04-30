6de40ba7990381a52e93d4ba329804e523d60eb6
raywu <raywu@aaeon.com.tw>
2020-04-30 15:44:52 +0800

    [DEBUG] TRACE ImageStart

--- AmiChipsetModulePkg/Library/NbPciDxeInitLib/NbPciDxeInitLib.c
+++ AmiChipsetModulePkg/Library/NbPciDxeInitLib/NbPciDxeInitLib.c
@@ -304,9 +304,11 @@ DEBUG_RAYDEBUG((-1, "(UINT32 *)ImageStart = 0x%X\n", (UINT32 *)ImageStart));
 DEBUG_RAYDEBUG((-1, "(UINT32 **)ImageStart = 0x%X\n", (UINT32 **)ImageStart));
 DEBUG_RAYDEBUG((-1, "((UINT32)ImageStart + 1) = 0x%X\n", ((UINT32)ImageStart + 1)));
 {
-  UINT32 Data32 ;
+  UINT32 Data32, *Pointer32 ;
 
   Dat32 = (UINT32)ImageStart ;
+  *Pointer32 = (UINT32)ImageStart ;
+  DEBUG_RAYDEBUG((-1, "**Pointer32 = 0x%X\n", **Pointer32));
 }
 /*
 
