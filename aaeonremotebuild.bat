3a7f162952b2c3cb622805dc380962ac700dc64f
raywu <raywu@aaeon.com.tw>
2020-11-24 10:33:00 +0800

    Remote Build Configurations

--- aaeonremotebuild.bat
+++ aaeonremotebuild.bat
@@ -21,7 +21,7 @@ echo -------------------------------------------
 @set SHA_FOLDER=%2
 @set OUTPUT_DIR=\\AA-LA11-1140B\_RemoteBuildFiles
 @set USERNAME=raywu@aaeon.com.tw
-@set PASSWORD=Aaeon1629x
+@set PASSWORD=Aaeon1629d
 
 :SetVebParm
 @set VEB=Kabylake
